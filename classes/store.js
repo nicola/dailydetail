4ed7fad8c7bce612fb0d476031e103b13a64c63a
Nicola Greco <notsecurity@gmail.com>
2013-05-29 01:27:00 +0100

    fixing same user issssue

--- classes/store.js
+++ classes/store.js
@@ -9,4 +9,4 @@ if (process.env.REDISTOGO_URL) {
 	store = redis.createClient();
 }
 exports.Redis = store;
-exports.MongoDB = mongoose.connect(process.env.MONGOHQ_URL || "mongodb://localhost/daily4");
+exports.MongoDB = mongoose.connect(process.env.MONGOHQ_URL || "mongodb://localhost/daily5");
